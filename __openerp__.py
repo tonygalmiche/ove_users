26352a9d65e3a9234e3f5c681e280efc8ac93b91
Tony Galmiche <tony.galmiche@infosaone.com>
2015-02-19 15:20:07 +0100

    Mise en ligne

---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'OVE - Users',
+    'version': '1.0',
+    'category': 'InfoSaône',
+    'description': """
+OVE - Users
+""",
+    'author': 'Tony GALMICHE',
+    'maintainer': 'InfoSaone',
+    'website': 'http://www.infosaone.com',
+    'depends': ['base'],
+    'data': ["res_users_view.xml",
+             ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
